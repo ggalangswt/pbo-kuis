9bd117981cf373b139e358ae6318ff9195303668
Galang Swastika Ramadhan <swastikaramadhan05@gmail.com>
2025-09-24 15:21:42 +0700

    fix fare

--- fare-calculator.cpp
+++ fare-calculator.cpp
@@ -1,3 +1,6 @@
+// Anggota Kelompok:
+// 1. Diaz Amantajati Susilo (24/545483/TK/60678)
+// 2. Galang Swastika Ramadhan (24/538251/TK/59692)
 #include <bits/stdc++.h>
 using namespace std;
 class Transport {
