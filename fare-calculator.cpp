bf60569fa538d711e8f2e77b1cb3f238b2c1e892
Galang Swastika Ramadhan <swastikaramadhan05@gmail.com>
2025-09-24 15:20:02 +0700

    create calculator-fare

---
+++ fare-calculator.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Transport {
+    public:
+    virtual int fare(int d) const = 0;
+    virtual ~Transport() = default;
+    };
+class Car : public Transport {
+    public:
+    int fare(int d) const override {
+        return 100 + 10 * d;
+    }
+};
+class Bus : public Transport {
+    public:
+    int fare(int d) const override {
+        return 50 + 8 * d;
+    }
+};
+class Train : public Transport {
+    public:
+    int fare(int d) const override {
+        if (d <= 50) return 200 + 3 * d;
+        return 200 + 3 * 50 + 2 * (d - 50);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Q; cin >> Q;
+    while (Q--) {
+        string type; int d; cin >> type >> d;
+
+        unique_ptr<Transport> t;
+        if (type == "CAR") t = make_unique<Car>();
+        else if (type == "BUS") t = make_unique<Bus>();
+        else if (type == "TRAIN") t = make_unique<Train>();
+        cout << t->fare(d) << "\n";
+    }
+    return 0;
+}
